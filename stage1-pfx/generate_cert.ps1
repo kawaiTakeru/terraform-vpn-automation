84e90a6976384f1df0a865ba8c27d6935e81a7ce
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-29 09:04:47 +0900

    Update generate_cert.ps1

--- stage1-pfx/generate_cert.ps1
+++ stage1-pfx/generate_cert.ps1
@@ -1,1 +1,32 @@
+# variables
+$opensslPath = "openssl"  # PATHが通っていればコマンド名のみでOK
+$certDir = "$(Build.ArtifactStagingDirectory)/certs"
+$jsonFile = "$(System.DefaultWorkingDirectory)/stage1-pfx/vars.json"
+
+# create output dir
+New-Item -ItemType Directory -Force -Path $certDir | Out-Null
+
+# load users from vars.json
+$json = Get-Content $jsonFile | ConvertFrom-Json
+
+foreach ($user in $json.users) {
+    $userName = $user.name
+    $password = $user.password
+    $keyFile = "$certDir/$userName.key"
+    $csrFile = "$certDir/$userName.csr"
+    $crtFile = "$certDir/$userName.crt"
+    $pfxFile = "$certDir/$userName.pfx"
+
+    # Create private key
+    & $opensslPath genrsa -out $keyFile 2048
+
+    # Create CSR
+    & $opensslPath req -new -key $keyFile -out $csrFile -subj "/CN=$userName"
+
+    # Self-sign certificate
+    & $opensslPath x509 -req -in $csrFile -signkey $keyFile -out $crtFile -days 365
+
+    # Create .pfx (with password)
+    & $opensslPath pkcs12 -export -out $pfxFile -inkey $keyFile -in $crtFile -password pass:$password
+}
 
