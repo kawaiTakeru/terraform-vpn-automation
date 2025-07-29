2407fa930cfa6966bf90658328a65c5de2df8d29
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-29 10:41:19 +0900

    Update get_profile.ps1

--- stage2-azurevpn/get_profile.ps1
+++ stage2-azurevpn/get_profile.ps1
@@ -8,8 +8,11 @@ $profileZip = "$outputDir/vpnprofile.zip"
 # 出力先作成
 New-Item -ItemType Directory -Force -Path $outputDir | Out-Null
 
+# 拡張機能の追加（初回のみ）
+& az extension add --name vpn-gateway
+
 # VPNクライアント構成ファイルの生成
-& az network vpn-client generate `
+& az network vpn-gateway vpn-client generate `
     --resource-group $resourceGroup `
     --name $gatewayName `
     --processor-architecture Amd64 `
@@ -17,6 +20,16 @@ New-Item -ItemType Directory -Force -Path $outputDir | Out-Null
     --output json `
     > $profileMetadata
 
-# ZIP URLの取得とダウンロード
-$zipUrl = (Get-Content $profileMetadata | ConvertFrom-Json).profileUrl
-Invoke-WebRequest -Uri $zipUrl -OutFile $profileZip
+# ZIP URLの取得
+if (Test-Path $profileMetadata) {
+    $zipUrl = (Get-Content $profileMetadata | ConvertFrom-Json).profileUrl
+
+    if (![string]::IsNullOrWhiteSpace($zipUrl)) {
+        # ZIPダウンロード
+        Invoke-WebRequest -Uri $zipUrl -OutFile $profileZip
+    } else {
+        Write-Error "profileUrl is empty. VPN Gateway から URL を取得できませんでした。"
+    }
+} else {
+    Write-Error "metadata JSON ファイルが作成されていません。"
+}
