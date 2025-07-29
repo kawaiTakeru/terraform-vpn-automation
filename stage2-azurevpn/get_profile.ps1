529b39292c4b9ca8f174b363e3ab4ab1640a80ba
kawaiTakeru <t-kawai@bfts.co.jp>
2025-07-29 09:05:01 +0900

    Update get_profile.ps1

--- stage2-azurevpn/get_profile.ps1
+++ stage2-azurevpn/get_profile.ps1
@@ -1,1 +1,22 @@
+# リソース情報
+$resourceGroup = "rg-test-hubnw-prd-jpe-001"
+$gatewayName = "vpngw-test-hubnw-prd-jpe-001"
+$outputDir = "$(Build.ArtifactStagingDirectory)/vpn"
+
+# 出力先作成
+New-Item -ItemType Directory -Force -Path $outputDir | Out-Null
+
+# ZIP生成（.azurevpn含む）
+az network vpn-client generate \
+  --resource-group $resourceGroup `
+  --name $gatewayName `
+  --processor-architecture Amd64 `
+  --authentication-method EAPTLS `
+  --output json > "$outputDir/profile_metadata.json"
+
+# ZIP URLの取得
+$zipUrl = (Get-Content "$outputDir/profile_metadata.json" | ConvertFrom-Json).profileUrl
+
+# ZIPダウンロード
+Invoke-WebRequest -Uri $zipUrl -OutFile "$outputDir/vpnprofile.zip"
 
